4b0d90d4df2f14a678dae9b77fbe05ac8195e652
platniumboi <platniumsableye@gmail.com>
2026-01-21 11:44:13 -0500

    new assignment

---
+++ MovieReviews.py
@@ -0,0 +1,11 @@
+class movie:
+    def __init__ (title, reviews[]):
+        Title = title
+        ReviewsForThisTitle = reviews
+    def 
+class reviews:
+    def __init__ (rating, textreview, movie):
+        Score = rating
+        prose = textreview
+        
+    
