7b24e92bb929f71c4c16030b573010e42e1c3b1a
platniumboi <platniumsableye@gmail.com>
2026-01-20 21:32:08 -0500

    should be done

--- BlackJackSolo.py
+++ BlackJackSolo.py
@@ -4,6 +4,27 @@ def draw_card(hand, deck):
 
     hand.append(deck.pop())  # take last card from deck
 
+def calculate_score(hand):
+        score = 0
+        for i in range(len(hand)):
+            if hand[i] == "J" or hand[i] == "Q" or hand[i] == "K":
+                score = score + 10
+            elif hand[i] == "A":
+                if score > 11:
+                    score = score + 1
+                else:
+                    score = score + 11
+            else:
+                score = score + int(hand[i])
+        if score > 21:
+            return "bust"
+        else:
+            return score
+
+def print_status(player):
+    for i in range(len(player)):
+        print(player[i])
+ 
  
 
 def main():
@@ -14,35 +35,73 @@ def main():
 
     dealer_hand = []
 
- 
+    active = True
+    able_to_draw = True
+    dealer_wants_to_draw = True
 
     # shuffle the deck
 
     random.shuffle(deck)
 
-    ...
+    # draw starting cards
+    draw_card(player_hand, deck)
+    draw_card(player_hand, deck)
+    draw_card(dealer_hand, deck)
+    draw_card(dealer_hand, deck)
 
-         # more stuff here; draw starting cards and start the game loop
-    def draw_card(player):
-        player.append(deck[-1])
-        deck.pop()
-    def calculate_score(hand):
-        score = 0
-        for i in hand:
-            if hand[i] == "J" or hand[i] == "Q" or hand[i] == "K":
-                score = score + 10
-            elif hand[i] == "A":
-                if score > 11:
-                    score = score + 1
-                else:
-                    score = score + 11
+    while active == True:
+        print("Your hand:")
+        print_status(player_hand)
+        print("Dealer's hand:")
+        print_status(dealer_hand)
+        print("")
+        if able_to_draw == True:
+            player_input = input("Do you want to hit or stand? (h/s): ")
+            print("")
+            if player_input == "h":
+                draw_card(player_hand, deck)
+            elif player_input == "s":
+                able_to_draw = False
+            if calculate_score(player_hand) == "bust":
+                print("")
+                print("Your hand:")
+                print_status(player_hand)
+                print("Dealer's hand:")
+                print_status(dealer_hand)
+                print("")
+                print("You busted! Dealer wins!")
+                active = False
+        elif calculate_score(player_hand) <= 21 and able_to_draw == False:
+            print("You chose to stand with a score of", calculate_score(player_hand))
+            able_to_draw = False
+        
+        if dealer_wants_to_draw and calculate_score(dealer_hand) != "bust" and calculate_score(dealer_hand) < 17:
+            draw_card(dealer_hand, deck)
+        elif calculate_score(dealer_hand) == "bust":
+            print("")
+            print("Your hand:")
+            print_status(player_hand)
+            print("Dealer's hand:")
+            print_status(dealer_hand)
+            print("")
+            print("Dealer busted! You win!")
+            active = False
+        elif calculate_score(dealer_hand) >= 17:
+            dealer_wants_to_draw = False
+        if able_to_draw == False and dealer_wants_to_draw == False:
+            player_score = calculate_score(player_hand)
+            dealer_score = calculate_score(dealer_hand)
+            if player_score > dealer_score:
+                print("You win!")
+            elif dealer_score > player_score:
+                print("Dealer wins!")
             else:
-                score = score + hand[i]
-    def print_status(player):
-        for i in player:
-            print(player[i])
-        print(calculate_score(player))
- 
+                print("It's a tie!")
+            active = False
+
+
+    # more stuff here; draw starting cards and start the game loop
+
 
 if __name__ == "__main__":
 
