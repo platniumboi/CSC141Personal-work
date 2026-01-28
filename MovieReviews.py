7a16f7079f56978efeab155f4ed2d41beed8fb46
platniumboi <platniumsableye@gmail.com>
2026-01-27 19:30:40 -0500

    should be it

--- MovieReviews.py
+++ MovieReviews.py
@@ -3,27 +3,41 @@
 class reviews:
     Score = int()
     prose = str()
-    def __init__ (rating, textreview):
-        reviews.Score = rating
-        reviews.prose = str(textreview)
+    def __init__ (self, rating, textreview):
+        self.Score = rating
+        self.prose = str(textreview)
+    def printreview(self):
+        print("Score:", self.Score)
+        print("Review:", self.prose)
 
 class movie:
     Title = str()
     ReviewsForThisTitle = []
-    def __init__ (title, reviews):
-        movie.Title = title
-        movie.ReviewsForThisTitle.append(reviews)
+    def __init__ (self, title, reviews):
+        self.Title = title
+        self.ReviewsForThisTitle.append(reviews)
 
-    def addreview(review):
-        movie.ReviewsForThisTitle.append(review)
-
-    def worst_review():
+    def addreview(self,review):
+        self.ReviewsForThisTitle.append(review)
+    def worst_review(self):
         x= 0
-        for i in range(len(movie.ReviewsForThisTitle)):
-            if movie.ReviewsForThisTitle[i].Score <= x:
+        for i in range(len(self.ReviewsForThisTitle)):
+            if self.ReviewsForThisTitle[i].Score <= x:
                 x=i
-        print(movie.ReviewsForThisTitle[x])
-            
+        self.ReviewsForThisTitle[x].printreview()
+    def best_review(self):
+        x= 0
+        for i in range(len(self.ReviewsForThisTitle)):
+            if self.ReviewsForThisTitle[i].Score >= x:
+                x=i
+        self.ReviewsForThisTitle[x].printreview()
+    def average_review(self):
+        total = 0
+        for i in range(len(self.ReviewsForThisTitle)):
+            total = total + self.ReviewsForThisTitle[i].Score
+        average = total / len(self.ReviewsForThisTitle)
+        print("Average score:", average)
+
 
 def main():
     moviereview = reviews(5, "a GOAT movie")
@@ -31,11 +45,13 @@ def main():
 
     anotherreview = reviews(2, "a garbage movie")
 
+    newMovie.addreview(moviereview)
     newMovie.addreview(anotherreview)
 
-    print(movie.Title)
-    print(movie.ReviewsForThisTitle)
-
+    print(newMovie.Title)
+    newMovie.worst_review()
+    newMovie.best_review()
+    newMovie.average_review()
 
 if __name__ == "__main__":
 
